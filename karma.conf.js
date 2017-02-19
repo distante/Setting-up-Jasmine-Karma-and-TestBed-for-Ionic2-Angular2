b8a94795494ff21e23324d2244af3d4145c0564e
distante <distante@ymail.com>
2017-02-19 19:31:24 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,30 @@
 // Karma configuration
-// Generated on Sun Feb 19 2017 16:45:40 GMT+0100 (Hora estándar romance)
+// https://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
     config.set({
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
 
+        //! ahmm I do not know, some plugins...
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-remap-istanbul'),
+            require('karma-mocha-reporter'),
+            require('angular-cli/plugins/karma')
+        ],
+
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine' , 'angular-cli'],
 
 
         // list of files / patterns to load in the browser
         files: [
+            { pattern: './src/test.ts', watched: false }
         ],
 
 
@@ -26,14 +36,30 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
+            './src/test.ts': ['angular-cli']
+        },
+        // This also do not know
+
+        mime: {
+            'text/x-typescript': ['ts','tsx']
+        },
+        remapIstanbulReporter: {
+            reports: {
+                html: 'coverage',
+                lcovonly: './coverage/coverage.lcov'
+            }
+        },
+        angularCli: {
+            config: './angular-cli.json',
+            environment: 'dev'
         },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
-
+        // default reporters: ['progress'],
+        reporters: config.angularCli && config.angularCli.codeCoverage ?        ['mocha', 'karma-remap-istanbul'] : ['mocha'],
 
         // web server port
         port: 9876,
@@ -63,6 +89,6 @@ module.exports = function (config) {
 
         // Concurrency level
         // how many browser should be started simultaneous
-        concurrency: Infinity
-    })
-}
+        //concurrency: Infinity
+    });
+};
